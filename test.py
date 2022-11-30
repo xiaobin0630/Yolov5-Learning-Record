821001a6007d120534b917e2de5311754d59d951
WJL <1255453063@qq.com>
2022-11-30 18:06:49 +0800

    11.30 主要在test.py 中 练习马赛克数据预处理

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import os
 import torch
 import torch.nn
 import numpy as np
+import cv2
 import glob
 from tqdm import tqdm
-from utils.utils import get_classes,get_anchors
+# from utils.utils import get_classes,get_anchors
 from random import sample, shuffle
 from PIL import Image
 import datetime
-from utils.dataloader import YoloDataset,yolo_dataset_collate
+# from utils.dataloader import YoloDataset,yolo_dataset_collate
+from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 def rand(a = 0,b = 1):
     return np.random.rand() * (b - a) + a
@@ -43,12 +45,368 @@ class Data_test(object):
         print(cls.day)
         print(cls.month)
         print(cls.year)
+def get_classes(path):
+    with open(path,encoding='utf-8') as f:
+        classes = f.readlines()
+        classes_name = [i.split("\n")[0] for i in classes]
+    return classes_name,len(classes)
+def get_anchors(path):
+    with open(path,encoding='utf-8') as f:
+        anchors = f.readline()
+        # print(anchors,type(anchors))
+        anchor = [float(x) for x in anchors.split(",")]
+        anchor = np.array(anchor).reshape(-1,2)
+        return anchor,len(anchor)
+def cvtColor(image):
+    # 判断图像是否RBG图像 shape数是否是3 第三维数是否是3
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        return image
+    # 不是就直接转化为RBG图像
+    else:
+        image.convert('RBG')
+        return image
+class YoloDataset(Dataset):
+    def __init__(self,annotation_lines,input_shape,num_classes,anchors,anchors_mask,epoch_length,mosaci,mixup,
+                 mosaic_prob,mixup_prob,train,special_aug_ratio = 0.7):
+        super(YoloDataset, self).__init__()
+        self.annotation_lines = annotation_lines
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.anchors = anchors
+        self.anchors_mask = anchors_mask
+        self.epoch_length = epoch_length
+        self.mosaic = mosaic
+        self.mosaic_prob = mosaic_prob
+        self.mixup = mixup
+        self.mixup_prob = mixup_prob
+        self.train = train
+        self.special_aug_ratio = special_aug_ratio
+        self.epoch_now = -1
+        self.length = len(self.annotation_lines)
+        self.bbox_attrs = 5 + num_classes
+        self.threshold = 4
+    # 返回数据条数
+    def __len__(self):
+        return self.length
+
+    # 数据预处理过程
+    def __getitem__(self, item):
+        # 保证item在0-self.length之间
+        item = item % self.length
+
+        # 判断这次是否要进行马赛克数据增强
+        if self.mosaic and self.mosaic_prob < self.rand() and self.epoch_now < self.epoch_length * self.special_aug_ratio:
+            # 随机在提取三张图片
+            lines = sample(self.annotation_lines,3)
+            # 将本次的item数据也添加进去
+            lines.append(self.annotation_lines[item])
+            # 打乱列表的顺序
+            shuffle(lines)
+            # 进行马克思数据增强
+            image, box = self.get_random_data_with_Mosaic(lines,self.input_shape)
+
+        # 进行数据预处理
+        else:
+            pass
+    # 马赛克数据增强
+    def get_random_data_with_Mosaic(self,annotation_line,input_shape,jitter=0.3,hue=0.1,sat=0.7,val=0.4):
+        # 得到传入的高宽
+        h, w = input_shape
+        # 生成最小补偿x与y的 0.3 ~ 0.7随机数
+        min_offset_x = self.rand(0.3,0.7)
+        min_offset_y = self.rand(0.3,0.7)
+        # 保存图像数据的列表
+        image_datas = []
+        # 保存坐标框的列表
+        box_datas = []
+        # 标记正在处理第几张图像
+        index = 0
+        # 循环单独对每张图像进行处理
+        for line in annotation_line:
+            # 对line进行分割
+            line_content = line.split()
+
+            # 读取图像
+            image = Image.open(line_content[0])
+
+            # 将图像换化为RBG图像
+            image = cvtColor(image)
+
+            # 获取本轮循环的图像高宽
+            iw, ih = image.size
+
+            # 保存框的位置 [[x1, y1, x2, y2, class]...]
+            box = np.array([np.array(list(map(int,box.split(',')))) for box in line_content[1:]])
+
+            # 生成随机数判断是否进行图像的左右翻转
+            flip = self.rand() < 0.5
+            if flip and len(box) > 0:
+                # 图像左右翻转
+                image = image.transpose(Image.FLIP_LEFT_RIGHT)
+                # 进行图像的左右翻转后,真实框的y轴不变化,x要做处理
+                '''
+                0,0 -----------> x (width)
+                 |
+                 |     
+                 | (x11,y11)                    (x1,y1)
+                 |   ---------                   ---------
+                 |  |         |                 |         | 
+                 |  |         |                 |         |
+                 |   ---------                   ---------
+                 |         (x22,y22)                       (x2,y2) 
+                 y
+              (height)
+                           x11 = w - x2
+                           x22 = w - x1
+                '''
+                box[:,[0,2]] = iw - box[:,[2,0]]
+            # 对图像进行缩放且进行长宽的扭曲
+            new_ar = iw/ih * self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter)
+            scale = self.rand(0.4,1)
+            if new_ar < 1:
+                nh = int(scale*h)
+                nw = int(nh*new_ar)
+            else:
+                nw = int(scale*w)
+                nh = int(nw/new_ar)
+            # 对图像进行resize(nw,nh)的大小
+            image = image.resize((nw,nh),Image.BICUBIC)
+            # 将图像进行放置,分别对应四张分割图片的位置
+
+            # 这里操作是为已经预处理的图像进行计算粘贴坐标点的操作
+            # 具体是这样:
+            # 在高宽为h,w的新图像中,0.3 ~ 0.7 的随机数各自乘以w与h,计算出来一个点
+            # 将这个点对应不同的第几张图像有不同的减法操作,计算出对应的粘贴点
+            if index == 0:
+                dx = int(w*min_offset_x) - nw # 64
+                dy = int(h*min_offset_y) - nh # 201
+            elif index == 1:
+                dx = int(w*min_offset_x) - nw
+                dy = int(h*min_offset_y)
+            elif index == 2:
+                dx = int(w*min_offset_x)
+                dy = int(h*min_offset_y)
+            elif index == 3:
+                dx = int(w*min_offset_x)
+                dy = int(h*min_offset_y) - nh
+
+
+            # 创建一张新图片
+            new_image = Image.new('RGB',(w,h),(128,128,128))
+            # 将image黏贴到new_image的(dx,dy)上
+            new_image.paste(image,(dx,dy))
+            # 将图像数据变为array
+            image_data = np.array(new_image)
+            # index加1
+            index = index + 1
+            # 零时存放列表
+            box_data = []
+            # 图像做了变化,真实框也要进行相关的变化
+            # 是否有真实框,有就做处理
+            if len(box)>0:
+                np.random.shuffle(box)
+                # 做与图像相同高宽缩放然后在加上对应的dx dy
+                box[:,[0,2]] = box[:,[0,2]] * nw/iw + dx
+                box[:,[1,3]] = box[:,[1,3]] * nh/ih + dy
+                # 对出界的坐标点继续规范处理
+                # 左上角坐标小于0则全部设置为0
+                box[:,0:2][box[:,0:2] < 0] = 0
+                # 右下角坐标大于图像的new_image图像的宽高,则对应设置为w,h
+                box[:,2][box[:,2]>w] = w
+                box[:,3][box[:,3]>h] = h
+                # 求框的宽高
+                box_w = box[:,2] - box[:,0]
+                box_h = box[:,3] - box[:,1]
+                # 保留真实框的宽高大于于1框
+                box = box[np.logical_and(box_w>1,box_h>1)]
+                # 创建一个与box同样大小的zeros矩阵 用于保存处理好的真实框
+                box_data = np.zeros((len(box),5))
+                # 存入真实框
+                box_data[:,len(box)] = box
+            # 将每次循环处理好的图像与真实框保存在image_datas与box_datas 对应列表中
+            image_datas.append(image_data)
+            box_datas.append(box_data)
+        # 之所以要做这样的操作是因为,之前那个循环操作最后处理好的图像 都是单独保存的
+        # 将图像分割后,放在一起
+        # 这里计算的那个粘贴点之前的原点
+        cutx = int(w * min_offset_x)
+        cuty = int(h * min_offset_y)
+
+        # 创建一个shape为(h,w,3)的新零矩阵
+        new_image = np.zeros([h, w, 3])
+        # 高在前 宽在后
+        # 第一张图片 image_datas[0][:cuty,:cutx,:] 赋值到new_image[:cuty,:cutx,:]
+        new_image[:cuty,:cutx,:] = image_datas[0][:cuty,:cutx,:]
+        # 第二张图片
+        new_image[cuty:,:cutx,:] = image_datas[1][cuty:,:cutx,:]
+        # 第三张图片
+        new_image[cuty:,cutx:,:] = image_datas[2][cuty:,cutx:,:]
+        # 第四张图片
+        new_image[:cuty,cutx:,:] = image_datas[3][:cuty,cutx:,:]
+
+        # 设置数据类型
+        new_image = np.array(new_image,np.uint8)
+
+        # 在[-1,1)均匀分布中采样3次 之后在进行对应位置相乘再加1 [hue,sat,val] 0.1 0.7 0.4
+        r = np.random.uniform(-1,1,3) * [hue,sat,val] + 1
+
+        # 将new_image 转为HSV 后进行通道分割
+        hue,sat,val = cv2.split(cv2.cvtColor(new_image,cv2.COLOR_RGB2HSV))
+        # 暂存数据类型
+        dtype = new_image.dtype
+        # 生成0到255的array数据,且类型为uint8
+        x = np.array(0,256,dtype=r.dtype)
+        # 生成对应通过的 hue sat val
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        # 融合
+        new_image = cv2.merge((cv2.LUT(hue,lut_hue),cv2.LUT(sat,lut_sat),cv2.LUT(val,lut_val)))
+        # 将hsv图像转化为RGB图像
+        new_image = cv2.cvtColor(new_image,cv2.COLOR_HSV2RGB)
+
+        # 最后再对真实框进行处理
+        new_boxes = self.merge_bboxes(box_datas,cutx,cuty)
+
+        return new_image,new_boxes
+    def merge_bboxes(self,bboxes,cutx,cuty):
+        # 最后保存真实框
+        merge_bbox = []
+        # 按图片的个数进行循环
+        for i in range(len(bboxes)):
+            # 提取第i张图像对应真实框
+            for box in bboxes[i]:
+                # 暂时存储真实框变量
+                temp_box = []
+                # 提取出坐标点值
+                x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+                # 第一张图像对应的真实框的处理
+                if i == 0:
+                    # 真实框左上角的坐标点已经大于了图像的右下角的坐标点就不要了这个框了
+                    if y1 > cuty or x1 > cutx:
+                        continue
+                    # 有一部分在对应图像的内部,要进行相应的处理
+                    # 真实框右下角的y的值大于了图像右下角的y的值 且真实框左上角的y的值小于图像右下角的y的值,则将y2设置为
+                    # 图像右下角的y的值
+                    if y2 >= cuty and y1 <= cuty:
+                        y2 = cuty
+                    # 理由同上
+                    if x2 >= cutx and x1 <= cutx:
+                        x2 = cutx
+                # 第二张图像对应的真实框处理
+                if i == 1:
+                    # 当真实框右下角坐标点y小于了cuty 或者左上角x的坐标x 大于了cutx 则说明真实完全出界,得抛弃这个坐标点
+                    if y2 < cuty or x1 > cutx:
+                        continue
+                    # 有一部分在对应图像的内部,要进行相应的处理
+                    # 真实框左下角上角坐标y在图像内部,右上角坐标y出界,用图像的右上角坐标cuty赋值
+                    if y2 >= cuty and y1 <= cuty:
+                        y1 = cuty
+                    # 真实框右下角x坐标值出界,右上角的x坐标在图像内部,得用图像的cutx给x2赋值
+                    if x2 >= cutx and x1 <= cutx:
+                        x2 = cutx
+                # 第三图片对应的真实框处理
+                if i == 2:
+                    # 图像的范围(cuty:,cutx:)
+                    # 如果真实框标签右下角x,y值小于cutx,cuty 则不要这个框
+                    if y2 < cuty or x2 < cutx:
+                        continue
+                    # 真实框右下角的y大于cuty 而左上角的y小于cuty 则用cuty赋值给y1
+                    if y2 >= cuty and y1 <= cuty:
+                        y1 = cuty
+                    # 真实框右下角的x大于cutx 而左上角的x 小于cutx 则用cutx 赋值给x1
+                    if x2 >= cutx and x1 <= cutx:
+                        x1 = cutx
+
+                if i == 3:
+                    # 图像的范围(:cuty,cutx:)
+                    # 如果真实框的左上角的y大于cuty 或者 真实框的右下角的x小于cutx 则不要这个框
+                    if y1 > cuty or x2 < cutx:
+                        continue
+                    # 真实框的右下角的y大于cuty 且真实框的左上角的y小于cuty 则用cuty赋值给真实框的右下角的y
+                    if y2 >= cuty and y1 <= cuty:
+                        y2 = cuty
+                    # 真实框的右下角的x大于cutx 且 真实框的左上角的x 小于cutx 则用cutx 赋值给真实框的左上角的x
+                    if x2 >= cutx and x1 <= cutx:
+                        x1 = cutx
+
+                # 暂保存处理好的真实框坐标
+                temp_box.append(x1)
+                temp_box.append(y1)
+                temp_box.append(x2)
+                temp_box.append(y2)
+                # 保存类类
+                temp_box.append(box[-1])
+                # 保存
+                merge_bbox.append(temp_box)
+        return merge_bbox
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # 生成[a,b)的随机数
+    def rand(self,a=0,b=1):
+        return np.random.rand()*(b-a) + a
 
 if __name__ == '__main__':
-    a1 = [3, 5, 2, 1, 6]
-    a2 = [7, 1, 2, 4, 2]
-    a = np.maximum(a1, 1)
-    print(a)
+    classes_path = "model_data/voc_classes"
+    classes_name,classes_num = get_classes(classes_path)
+    # print(classes_name,classes_num)
+    input_shape = [640,640]
+    anchors_path = "model_data/yolo_anchors"
+    anchors,anchors_num = get_anchors(anchors_path)
+    # print(anchors,anchors)
+    anchors_mask = [[6,7,8],[3,4,5],[0,1,2]]
+    epoch_length = 20
+    mosaic = True
+    mosaic_prob = 0.5
+    mixup = True
+    mixup_prob = 0.5
+    special_aug_ratio = 0.7
+    r = np.random.uniform(-1, 1, 3) * [0.1, 0.7, 0.4] + 1
+
+    print(r,type(r))
+    # lines = [1,2,3,4,5,6]
+    # l = sample(lines,3)
+    # print(l,type(l))
+    # l.append(8)
+    # print(l,type(l))
+    # shuffle(l)
+    # print(l,type(l))
+
+
+    # b = 10
+    # a = 0
+    # s = np.random.rand() * (b - a) + a
+    # print(s)
+    # a1 = [3, 5, 2, 1, 6]
+    # a2 = [7, 1, 2, 4, 2]
+    # a = np.maximum(a1, 1)
+    # print(a)
 
     # ground_truth_files_list = glob.glob('model_data' + '/*.txt')
     # id = ground_truth_files_list[0].split('.txt',1)[0]
